b18f199794debbfa7e570635a9e1ddf3e337843c
Felix Roth <git@felixxroth.de>
2020-03-02 22:04:57 +0100

    removed log iptions

--- main.js
+++ main.js
@@ -36,8 +36,8 @@ class EigeneDatenpunkte extends utils.Adapter {
 
 		// The adapters config (in the instance object everything under the attribute "native") is accessible via
 		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
+		//this.log.info("config option1: " + this.config.option1);
+		//this.log.info("config option2: " + this.config.option2);
 
 
 		
