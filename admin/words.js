a9b572320a5562f0556346ef3a8219a6741cedf7
Felix Roth <git@felixxroth.de>
2020-03-03 20:48:04 +0100

    added option3 tranlsation

--- admin/words.js
+++ admin/words.js
@@ -4,38 +4,18 @@
 systemDictionary = {
 	"eigene_datenpunkte adapter settings": {
 		"en": "Adapter settings for eigene_datenpunkte",
-		"de": "Adaptereinstellungen für eigene_datenpunkte",
-		"ru": "Настройки адаптера для eigene_datenpunkte",
-		"pt": "Configurações do adaptador para eigene_datenpunkte",
-		"nl": "Adapterinstellingen voor eigene_datenpunkte",
-		"fr": "Paramètres d'adaptateur pour eigene_datenpunkte",
-		"it": "Impostazioni dell'adattatore per eigene_datenpunkte",
-		"es": "Ajustes del adaptador para eigene_datenpunkte",
-		"pl": "Ustawienia adaptera dla eigene_datenpunkte",
-		"zh-cn": "eigene_datenpunkte的适配器设置"
+		"de": "Adaptereinstellungen für eigene_datenpunkte"
 	},
 	"option1": {
 		"en": "option1",
-		"de": "Option 1",
-		"ru": "Опция 1",
-		"pt": "Opção 1",
-		"nl": "Optie 1",
-		"fr": "Option 1",
-		"it": "opzione 1",
-		"es": "Opción 1",
-		"pl": "opcja 1",
-		"zh-cn": "选项1"
+		"de": "Option 1"
 	},
 	"option2": {
 		"en": "option2",
-		"de": "Option 2",
-		"ru": "option2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
+		"de": "Option 2"
+	},
+	"option3": {
+		"en": "option3",
+		"de": "Option 3"
 	}
 };
