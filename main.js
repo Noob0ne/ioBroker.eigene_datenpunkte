6d31c8cccbb0ce26311de2df8215f98d12041a45
Felix Roth <git@felixxroth.de>
2020-03-07 11:29:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -168,6 +168,7 @@ class EigeneDatenpunkte extends utils.Adapter {
 			}
 
 			async function createVariable(name){
+				this.log.info("Funktion mit "+name+" aufgerufen");
 				this.setObjectAsync(obj.message, {
 					type: "state",
 					common: {
